c1a8216f9d4caee3756f93a9613218ec0c9333f9
Robert Ellison <ellison@tatorcist.univ.pitt.edu>
2022-12-28 20:33:23 -0500

    Correct null check error

--- myname.py
+++ myname.py
@@ -1,11 +1,12 @@
 # myname.py
 # Author: Robert Ellison
 
-name = None
+
 
 # get_name function -- requests users name
 def get_name():
-    while name is None:
+    name = None
+    while not name:
         name=input("Please enter your name: ")
     return name
 
