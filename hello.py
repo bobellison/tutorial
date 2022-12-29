056fde8478256474c47f1513ff3591aebc509ed3
Bob Ellison <ellison@DESKTOP-IVRG900>
2022-12-28 21:19:37 -0500

    updated commenting throughout hello and myname scripts

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 # hello.py
 # Author: Robert Ellison
+# A small script to say hello from Git
 
 # load modules
 import myname
 
 
 # main code
+
+# call the get_name function from the myname module
 name = myname.get_name()
+
+#print our welcome message
 print("Hello {}".format(name),"from Git!")
--- myname.py
+++ myname.py
@@ -1,7 +1,7 @@
 # myname.py
 # myname request module
 # Author: Robert Ellison
-
+# module to request the users name
 
 
 # get_name function -- requests users name
